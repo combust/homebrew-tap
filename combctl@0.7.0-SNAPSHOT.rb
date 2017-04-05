0f49be01577f9b43447667caeb9ea0123b1ec808
Hollin Wilkins <hollinrwilkins@gmail.com>
2017-04-04 23:48:45 -0700

    Remove symlink for now.

--- combctl@0.7.0-SNAPSHOT.rb
+++ combctl@0.7.0-SNAPSHOT.rb
@@ -11,10 +11,5 @@ class CombctlAT070Snapshot < Formula
 
   def install
     prefix.install Dir["*"]
-    bin.install_symlink prefix/"combctl/bin/combctl"
-  end
-
-  test do
-    system "#{bin}/combctl help"
   end
 end
