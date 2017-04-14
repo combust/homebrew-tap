ce09b01a8680dfa42650e381fc3af08f534e1558
Hollin Wilkins <hollinrwilkins@gmail.com>
2017-04-14 15:48:16 -0700

    Update shasum.

--- combctl@0.7.0-SNAPSHOT.rb
+++ combctl@0.7.0-SNAPSHOT.rb
@@ -6,7 +6,7 @@ require 'erb'
 class CombctlAT070Snapshot < Formula
   homepage "github.com/combust/combust"
   url "https://s3-us-west-2.amazonaws.com/combust-cloud/releases/combctl-0.7.0-SNAPSHOT.tgz"
-  sha256 "4502341e44a873f8d35cdb7952bb92538a11842c9053c7a77306c6e3aab4a2c9"
+  sha256 "303e6587826142414d49a90b4e5743ff84a69af28e9fdecf72befb263c2c6a80"
   version "v0.7.0-SNAPSHOT"
 
   def install
