862073cd4edc61bf63eb52fbc50c853f3503ef4f
Hollin Wilkins <hollinrwilkins@gmail.com>
2017-06-19 13:18:40 -0700

    Update formula.

--- combctl@0.7.1-SNAPSHOT.rb
+++ combctl@0.7.1-SNAPSHOT.rb
@@ -3,7 +3,7 @@ require "language/go"
 require 'erb'
 
 
-class CombctlAT070Snapshot < Formula
+class CombctlAT071Snapshot < Formula
   homepage "github.com/combust/combust"
   url "https://s3-us-west-2.amazonaws.com/combust-cloud/releases/combctl-0.7.1-SNAPSHOT.tgz"
   sha256 "3a121be1f116a38c8ccb028eeac15ead730f2fe0fae949dbcf241d85b30921c5"
